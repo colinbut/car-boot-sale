7c3615bd42fb5790b2917e3a7adf41c74c349124
Colin But <colinbut@users.noreply.github.com>
2016-08-21 16:33:40 +0100

    added model item

--- src/main/java/com/mycompany/carbootsale/domain/Item.java
+++ src/main/java/com/mycompany/carbootsale/domain/Item.java
@@ -5,5 +5,65 @@
  */
 package com.mycompany.carbootsale.domain;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.util.Objects;
+
 public class Item {
+
+    private String name;
+    private String description;
+    private double price;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return Double.compare(item.price, price) == 0
+            && Objects.equals(name, item.name)
+            && Objects.equals(description, item.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, price);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+            .append("description", description)
+            .append("name", name)
+            .append("price", price)
+            .toString();
+    }
 }
