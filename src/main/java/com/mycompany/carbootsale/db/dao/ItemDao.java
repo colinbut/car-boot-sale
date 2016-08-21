7a24544ecf1dd05b96370a23b7a6b0d505ac7752
Colin But <colinbut@users.noreply.github.com>
2016-08-21 18:09:07 +0100

    spring jdbc dependency and in memory hsql db

---
+++ src/main/java/com/mycompany/carbootsale/db/dao/ItemDao.java
@@ -0,0 +1,9 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2016 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.carbootsale.db.dao;
+
+public interface ItemDao {
+}
