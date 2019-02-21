cb5937b6e7b48e4a4135104006d06e1d3a697cad
Colin But <colinbut@users.noreply.github.com>
2019-02-21 23:25:24 +0000

    Added kotlin skeleton

---
+++ kotlin/src/main/kotlin/com/mycompany/carbootsale/carbootsale/CarbootsaleApplication.kt
@@ -0,0 +1,11 @@
+package com.mycompany.carbootsale.carbootsale
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarbootsaleApplication
+
+fun main(args: Array<String>) {
+	runApplication<CarbootsaleApplication>(*args)
+}
