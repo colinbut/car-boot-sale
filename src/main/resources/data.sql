624fab6d906683de68e228cf014df82a5fcc71b6
Colin But <colinbut@users.noreply.github.com>
2019-02-21 23:23:34 +0000

    Moving java project to sub module project folder

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into item (name, description, price) values ('Jug', 'An old rusty jug', 1.1);
-insert into item (name, description, price) values ('Keyring', 'A keyring', 0.5);
-insert into item (name, description, price) values ('Plate', 'A set of plates', 20.0);
-insert into item (name, description, price) values ('Pack of Fork and Knifes', 'A pack of dining fork and knifes', 4.5);
-insert into item (name, description, price) values ('Hairdryer', 'Used hairdryer to dry your hair', 2.0);
-insert into item (name, description, price) values ('Computer Mouse', 'A computer mouse', 1.6);
